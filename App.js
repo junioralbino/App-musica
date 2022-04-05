04deaf6f6dfd3c891c93e32401dbd456ab21d58c
Junior albinol <junioralbino741@gmail.com>
2022-04-05 14:57:19 -0300

    incluido o css do estilo

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import react, { useState } from 'react';
 import {ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function App() {
+
+  const [audio, setarAudio ] = useState(null);
+
+  const [musicas, setarMusicas] = useState([
+     {
+        nome: 'Sweet Child O Mine',
+        artista: 'Guns N Roses',
+        playing: 'false',
+        file: ''
+     },
+     {
+      nome: 'Rocket Queen',
+      artista: 'Guns N Roses',
+      playing: 'false',
+      file: ''
+     },
+     {
+      nome: 'Numb',
+      artista: 'Linkin Park',
+      playing: 'true',
+      file: ''
+     }
+  ]);
+
   return (
       <ScrollView style={styles.container}>
         <StatusBar hidden />
         <View style={styles.header}>
-          <Text>Criando um app</Text>
+          <Text style={{textAlign: 'center', color:'white', fontSize: 24}}>Musica Star</Text>
+        </View>
+        
+        <View style={styles.table}>
+           <Text style={{width: '50%', color:'rgb(200, 200, 200)' }}>Música</Text>
         </View>
       </ScrollView>
   );
@@ -22,5 +51,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#1db954',
     width: '100%',
     padding: 20
+  },
+  table:{
+    flexDirection: 'row',
+    padding: 20,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1
   }
 });
