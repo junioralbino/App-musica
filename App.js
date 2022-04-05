b566f6d4866d415c94e7672982585e08828c15f8
Junior albinol <junioralbino741@gmail.com>
2022-04-05 13:32:48 -0300

    Criadno um app de musica curso da dankicode

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ScrollView style={styles.container}>
+        <StatusBar hidden />
+        <View style={styles.header}>
+           <Text style={{textAlign: 'center', color:'white', fontSize: 24}}>Music Star</Text>  
+        </View> 
+      </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#222'
   },
+  header:{
+    backgroundColor: '#1db954',
+    width: '100%',
+    padding: 20
+  }
 });
